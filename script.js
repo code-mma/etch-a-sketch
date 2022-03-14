70cbcc73b4e42777ddcccb4126d7d6ed2ab16d23
code-mma <michaelangelo.mma@gmail.com>
2022-03-15 00:38:45 +0800

    Modify working code of the 'Clear' button

--- script.js
+++ script.js
@@ -1,23 +1,59 @@
+let body = document.querySelector('body');
+let container = document.querySelector('container');
 
-const body = document.querySelector('body');
-const createButton = document.createElement('button')
+let createButton = document.createElement('button')
 createButton.setAttribute('id','clear-button');
 createButton.textContent = 'CLEAR';
 body.appendChild(createButton);
 
-for (i = 0; i < 16; i++) {
-    const container = document.querySelector('container');
-    const createSquare = document.createElement('div');
-    createSquare.classList.add('box');
-    container.appendChild(createSquare);
+let sideNum = 64;
+
+createGrid(sideNum);
+
+function createGrid (sideNum) {
+    for (j = 1; j <= sideNum; j++){
+        for (i = 1; i <= sideNum; i++) {
+            const createSquare = document.createElement('div');
+            createSquare.classList.add('box');
+            container.appendChild(createSquare);
+
+            if (i == sideNum) {
+                const createBreak = document.createElement('div');
+                createBreak.classList.add('break');
+                container.appendChild(createBreak);
+            }
+        }
+    }
+
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) => {
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = 'red';
+        });
+    });
 }
 
-var boxes = document.querySelectorAll('.box');
-boxes.forEach((box) => {
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'red';
-    });
-    createButton.addEventListener('click', () => {
-        box.style.backgroundColor = 'yellowgreen';
-    });
+createButton.addEventListener('click', () => {
+
+    do {
+        sideNum = prompt('Enter your desired unit length of side: ')
+
+        if (sideNum > 100) {
+            alert('Side length must not excess 100 units')
+        }
+        else {
+            createGridAgain();
+        }
+    }
+    while (sideNum > 100)
+    
+    function createGridAgain() {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        createGrid(sideNum);
+    }
 });
+
+
+
